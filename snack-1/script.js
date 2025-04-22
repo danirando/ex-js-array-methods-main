f5d953a6dc4a0b8befc8d914ed297d79c6e3eb30
danirando <danielerando@tiscali.it>
2025-04-22 13:26:07 +0200

    snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,6 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+console.log(names);
